8ae1414f017476ff28c98cadadb460c84824ea3e
babashun <08061656092p@gmail.com>
2023-01-15 17:38:25 +0900

    ダッシュボードの追加

---
+++ app/dashboards/tiktok_movie_log_dashboard.rb
@@ -0,0 +1,75 @@
+require "administrate/base_dashboard"
+
+class TiktokMovieLogDashboard < Administrate::BaseDashboard
+  # ATTRIBUTE_TYPES
+  # a hash that describes the type of each of the model's fields.
+  #
+  # Each different type represents an Administrate::Field object,
+  # which determines how the attribute is displayed
+  # on pages throughout the dashboard.
+  ATTRIBUTE_TYPES = {
+    id: Field::Number,
+    comment_count: Field::Number,
+    like_count: Field::Number,
+    share_count: Field::Number,
+    tiktok_movie: Field::BelongsTo,
+    view_count: Field::Number,
+    created_at: Field::DateTime,
+    updated_at: Field::DateTime,
+  }.freeze
+
+  # COLLECTION_ATTRIBUTES
+  # an array of attributes that will be displayed on the model's index page.
+  #
+  # By default, it's limited to four items to reduce clutter on index pages.
+  # Feel free to add, remove, or rearrange items.
+  COLLECTION_ATTRIBUTES = %i[
+    id
+    comment_count
+    like_count
+    share_count
+  ].freeze
+
+  # SHOW_PAGE_ATTRIBUTES
+  # an array of attributes that will be displayed on the model's show page.
+  SHOW_PAGE_ATTRIBUTES = %i[
+    id
+    comment_count
+    like_count
+    share_count
+    tiktok_movie
+    view_count
+    created_at
+    updated_at
+  ].freeze
+
+  # FORM_ATTRIBUTES
+  # an array of attributes that will be displayed
+  # on the model's form (`new` and `edit`) pages.
+  FORM_ATTRIBUTES = %i[
+    comment_count
+    like_count
+    share_count
+    tiktok_movie
+    view_count
+  ].freeze
+
+  # COLLECTION_FILTERS
+  # a hash that defines filters that can be used while searching via the search
+  # field of the dashboard.
+  #
+  # For example to add an option to search for open resources by typing "open:"
+  # in the search field:
+  #
+  #   COLLECTION_FILTERS = {
+  #     open: ->(resources) { resources.where(open: true) }
+  #   }.freeze
+  COLLECTION_FILTERS = {}.freeze
+
+  # Overwrite this method to customize how tiktok movie logs are displayed
+  # across all pages of the admin dashboard.
+  #
+  # def display_resource(tiktok_movie_log)
+  #   "TiktokMovieLog ##{tiktok_movie_log.id}"
+  # end
+end
