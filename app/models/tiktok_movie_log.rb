1448bbc84148934195f35ff46595a4160f101265
babashun <08061656092p@gmail.com>
2023-02-07 11:33:56 +0900

    Update tiktok_movie_log.rb

--- app/models/tiktok_movie_log.rb
+++ app/models/tiktok_movie_log.rb
@@ -1,3 +1,8 @@
 class TiktokMovieLog < ApplicationRecord
   belongs_to :tiktok_movie
+  has_one :campaign, through: :tiktok_movie
+
+  def create_reward
+    amount = view_count * campaign.play_unit_price
+  end
 end
