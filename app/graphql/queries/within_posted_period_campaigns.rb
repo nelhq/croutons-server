267b4b16ba53905661c67b0814df417969267aa6
babashun <08061656092p@gmail.com>
2023-03-03 18:38:28 +0900

    Update within_posted_period_campaigns.rb

--- app/graphql/queries/within_posted_period_campaigns.rb
+++ app/graphql/queries/within_posted_period_campaigns.rb
@@ -3,7 +3,7 @@ module Queries
     type Types::Objects::CampaignType.connection_type, null: true
 
     def resolve(**args)
-      ::Campaign.within_posted_period.order(posted_period: "DESC")
+      ::Campaign.within_posted_period.order(posted_period: "ASC")
     end
   end
 end
